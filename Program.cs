06a18647771dd44841a5e66dd1f0f6eb24ae8777
Whittley-chan <reinee066@gmail.com>
2026-04-13 11:20:59 +0700

    MKT-8 Final Review & Run #done

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.UseUrls("http://0.0.0.0:5094");
+
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
